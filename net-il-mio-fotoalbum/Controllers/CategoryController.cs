ae24484afbce805bb94624c73164adc3a868b065
Salvatore Esposito <esposito.00@outlook.it>
2023-05-17 11:22:10 +0200

    added CategoryFormModel and CategoryContrroller (with Index, Create and Delete)

---
+++ net-il-mio-fotoalbum/Controllers/CategoryController.cs
@@ -0,0 +1,78 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.Mvc.Rendering;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class CategoryController : Controller
+    {
+        public IActionResult Index()
+        {
+            using PhotoContext context = new();
+
+            var categoriesList = context.Categories.ToList();
+
+            return View(categoriesList);
+        }
+
+
+        //CREATE
+        [HttpGet]
+        public IActionResult Create()
+        {
+            using PhotoContext context = new();
+
+            //creazione model da passare alla pagina get
+            CategoryFormModel model = new();
+
+            model.Category = new Category();
+
+            return View("Create", model);
+        }
+
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Create(CategoryFormModel data)
+        {
+            if (!ModelState.IsValid)
+            {
+                return View("Create", data);
+            }
+
+            using PhotoContext context = new PhotoContext();
+
+            Category categoryCreate = new();
+            categoryCreate.Name = data.Category.Name;
+            categoryCreate.Description = data.Category.Description;
+
+            context.Categories.Add(categoryCreate);
+            context.SaveChanges();
+
+            return RedirectToAction("Index");
+        }
+
+
+
+        //DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(string name) //passo il name (che dovrà essere unico) dato che non ha un id
+        {
+            using PhotoContext context = new PhotoContext();
+            var categoryDelete = context.Categories.Where(c => c.Name == name).FirstOrDefault();
+
+            if (categoryDelete != null)
+            {
+                context.Categories.Remove(categoryDelete);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
---
+++ net-il-mio-fotoalbum/Models/CategoryFormModel.cs
@@ -0,0 +1,12 @@
+﻿using Microsoft.AspNetCore.Mvc.Rendering;
+
+namespace net_il_mio_fotoalbum.Models
+{
+    public class CategoryFormModel
+    {
+        public Category Category { get; set; }
+
+        public List<SelectListItem>? Photos { get; set; }
+        public List<string>? SelectPhotos { get; set; }
+    }
+}
