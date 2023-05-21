5e65879250fb52ef45b117dbd054557648d81f87
Salvatore Esposito <esposito.00@outlook.it>
2023-05-21 22:01:46 +0200

    general changes (added Index and Delete for Message, changed layout)

---
+++ net-il-mio-fotoalbum/Controllers/MessageController.cs
@@ -0,0 +1,42 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Microsoft.EntityFrameworkCore;
+using net_il_mio_fotoalbum.Areas;
+using net_il_mio_fotoalbum.Models;
+
+namespace net_il_mio_fotoalbum.Controllers
+{
+    public class MessageController : Controller
+    {
+        public IActionResult Index()
+        {
+            using PhotoContext context = new();
+
+            var messages = context.Messages.ToList();
+
+            return View(messages);
+        }
+
+
+        //DELETE
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public IActionResult Delete(int id)
+        {
+            using PhotoContext context = new();
+
+            var messageDelete = context.Messages.Where(m => m.Id == id).FirstOrDefault();
+
+            if (messageDelete != null)
+            {
+                context.Messages.Remove(messageDelete);
+                context.SaveChanges();
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+                return NotFound();
+            }
+        }
+    }
+}
